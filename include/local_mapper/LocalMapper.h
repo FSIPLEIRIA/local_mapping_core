1490d60fed491a7c6928c35c4c913934859c15b3
carlostojal <carlos.tojal@hotmail.com>
2023-06-02 00:28:45 +0100

    add some local mapper methods

--- include/local_mapper/LocalMapper.h
+++ include/local_mapper/LocalMapper.h
@@ -5,15 +5,43 @@
 #ifndef LOCAL_MAPPING_CORE_LOCALMAPPER_H
 #define LOCAL_MAPPING_CORE_LOCALMAPPER_H
 
-#include <vector>
 #include <local_mapper/vision/RGBCamera.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <mutex>
 
 namespace t24e::local_mapper {
 
+    /*! \brief Main class of the project. Receives images from several cameras and returns cone detections in 3D space. */
     class LocalMapper {
 
         private:
-            std::vector<local_mapper::vision::RGBCamera> cameras;
+            /*! \brief Map of camera's shared pointers indexed by a unique name. */
+            std::unordered_map<std::string,std::shared_ptr<local_mapper::vision::RGBCamera>> cameras;
+
+            /*! \brief Current map of cones. */
+            pcl::PointCloud<pcl::PointXYZL> currentMap;
+
+            /*! \brief Mutex which controls access to the cones map. */
+            std::mutex mapMutex;
+
+        public:
+            LocalMapper();
+            ~LocalMapper();
+
+            /*! \brief Register a color image on a camera object. */
+            void addColorImage(const std::string &cameraName, const cv::Mat &img);
+
+            /*! \brief Declare the transform between a camera and the car's base frame. */
+            void setCameraTf(const std::string& cameraName, const Eigen::Affine3d& tf);
+
+            // TODO: keep the hash of the cameras to avoid re-computing when all the images are the same as the last time.
+            /*! \brief Get the current cones map. Grabs the images from all the cameras, detects cones and
+             * does 3D reconstruction. */
+            pcl::PointCloud<pcl::PointXYZL> getCurrentMap() const;
     };
 
 } // local_mapper
--- src/LocalMapper.cpp
+++ src/LocalMapper.cpp
@@ -6,5 +6,40 @@
 
 namespace t24e {
     namespace local_mapper {
+        LocalMapper::LocalMapper() {
+
+        }
+
+        LocalMapper::~LocalMapper() {
+            // delete all camera pointers
+            for(auto & c : this->cameras) {
+                c.second.reset();
+            }
+        }
+
+        void LocalMapper::addColorImage(const std::string &cameraName, const cv::Mat &img) {
+
+            if(this->cameras.contains(cameraName)) {
+                this->cameras[cameraName]->captureColorImage(img);
+            } else {
+                std::shared_ptr<vision::RGBCamera> newCamera = std::make_shared<vision::RGBCamera>();
+                newCamera->captureColorImage(img);
+                this->cameras[cameraName] = newCamera;
+            }
+        }
+
+        void LocalMapper::setCameraTf(const std::string &cameraName, const Eigen::Affine3d& tf) {
+            if(this->cameras.contains(cameraName)) {
+                this->cameras[cameraName]->setTfToBase(tf);
+            } else {
+                std::shared_ptr<vision::RGBCamera> newCamera = std::make_shared<vision::RGBCamera>();
+                newCamera->setTfToBase(tf);
+                this->cameras[cameraName] = newCamera;
+            }
+        }
+
+        pcl::PointCloud<pcl::PointXYZL> LocalMapper::getCurrentMap() const {
+            return this->currentMap;
+        }
     } // t24e
 } // local_mapper
